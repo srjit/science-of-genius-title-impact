d7aa1498d0585919e65e0bd08741db6846ab3100
Sreejith Sreekumar <sreekumar.s@husky.neu.edu>
2019-02-05 19:09:11 +0000

    story building notebooks

--- src/mutual_information.py
+++ src/mutual_information.py
@@ -1,8 +1,16 @@
 import numpy as np
+import math
 
 
-def calculate(X,Y,bins):
+def get_number_of_bins(length):
+    ## optimal number of bins
+    ## https://stats.stackexchange.com/questions/179674/number-of-bins-when-computing-mutual-information
+    return math.floor(np.sqrt(length/5))
 
+
+def calculate(X,Y):
+    
+   bins = get_number_of_bins(len(X))
    c_XY = np.histogram2d(X,Y,bins)[0]
    c_X = np.histogram(X,bins)[0]
    c_Y = np.histogram(Y,bins)[0]
